cfcd6799cbdebf95f756bf8e040b994e5f196257
Julian Hautzmayer <j.hautzmayer@hautzy.com>
2024-06-21 22:13:01 +0200

    testing out irrft

--- test_irrft.py
+++ test_irrft.py
@@ -8,30 +8,20 @@ class IRFFTLayer(Layer):
         super(IRFFTLayer, self).__init__()
 
     def call(self, inputs):
-        if inputs.shape[0] is None:
-            real_tensor = tf.random.normal(shape=(513, 2))
-            imag_tensor = tf.random.normal(shape=(513, 2))
+        real_tensor = tf.random.normal(shape=(513, 2))
+        imag_tensor = tf.random.normal(shape=(513, 2))
 
-            inputs = tf.complex(real_tensor, imag_tensor)
-        # Apply IRFFT. The input is expected to be a complex tensor suitable for IRFFT
+        inputs = tf.complex(real_tensor, imag_tensor)
         return tf.signal.irfft(inputs)
 
 
-# Combine them into a complex tensor
-input_tensor = Input(shape=(2))
+input_tensor = Input(shape=(1,))
 custom_layer = IRFFTLayer()(input_tensor)  # Output is the result of the IRFFT
 
 model = tf.keras.Model(inputs=input_tensor, outputs=custom_layer)
 
-real_tensor = tf.random.normal(shape=(513, 2))
-imag_tensor = tf.random.normal(shape=(513, 2))
+model.build(input_shape=(None, 1))
 
-input = tf.complex(real_tensor, imag_tensor)
-
-res = model(input)
-
-print(res.shape)
-
-model.save('saved_model_irfft_only')
+model.save('./saved_model_irfft_only')
 
 print("Model saved")
