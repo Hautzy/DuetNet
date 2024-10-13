c29438b10e9856c15d9be73111863e81645b277f
Julian Hautzmayer <j.hautzmayer@hautzy.com>
2024-10-13 13:30:11 +0200

    Pypass building phase

--- waveform_model.py
+++ waveform_model.py
@@ -7,18 +7,6 @@ from models import Models_functions
 from utils import Utils_functions
 
 
-class GenerateWaveformLayer(Layer):
-    def __init__(self, gen_ema, dec, dec2, **kwargs):
-        super(GenerateWaveformLayer, self).__init__(**kwargs)
-        self.gen_ema = gen_ema
-        self.dec = dec
-        self.dec2 = dec2
-
-    @tf.function
-    def call(self, inputs):
-        return U.generate_waveform(inputs, self.gen_ema, self.dec, self.dec2, batch_size=64)
-
-
 export_folder = 'exported_models'
 
 args = parse_args()
@@ -29,6 +17,21 @@ M = Models_functions(args)
 M.download_networks()
 models_ls = M.get_networks()
 critic, gen, enc, dec, enc2, dec2, gen_ema, [opt_dec, opt_disc], switch = models_ls
+
+class GenerateWaveformLayer(Layer):
+    def __init__(self, gen_ema, dec, dec2, **kwargs):
+        super(GenerateWaveformLayer, self).__init__(**kwargs)
+        self.gen_ema = gen_ema
+        self.dec = dec
+        self.dec2 = dec2
+
+    @tf.function
+    def call(self, inputs):
+        if inputs.shape[0] is None:
+            print('Using dummy data to build the model...')
+            fac = (args.seconds // 23) + 1
+            inputs = U.get_noise_interp_multi(fac, args.truncation)
+        return U.generate_waveform(inputs, self.gen_ema, self.dec, self.dec2, batch_size=64)
 
 input_tensor = Input(shape=(256, 128))
 waveform_layer = GenerateWaveformLayer(gen_ema, dec, dec2)(input_tensor)
