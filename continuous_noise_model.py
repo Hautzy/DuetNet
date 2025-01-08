d232a03d7a81757573ca369bf885f61e5d32a57f
Julian Hautzmayer (Hautzy GmbH) <julian.hautzmayer@gmail.com>
2025-01-08 15:23:50 +0100

    Update continuous_noise_model.py

--- continuous_noise_model.py
+++ continuous_noise_model.py
@@ -36,7 +36,7 @@ def continuous_noise_interp(noiseg, right_noisel, fac=1, var=2.0):
     # rls = rls[:, : (rls.shape[-2] // args.latlen) * args.latlen, :]
     rls = tf.split(rls, rls.shape[-2] // args.latlen, -2)
     last_noisel = rls[0][:, -1, :]
-    print(last_noisel)
+    print(last_noisel.shape)
     return tf.concat(rls[:fac], 0), last_noisel
 
 class ContinuousNoiseLayer(Layer):
